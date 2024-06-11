5f469bc82b2b1096a0d0e87a506feed7d98e7c86
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-06-11 23:18:22 +0600

    Add permission grouping logic to HomeController for debugging purposes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\PermissionRegistrar;
 
 class HomeController extends Controller
 {
@@ -11,6 +12,20 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+
+        $permissions = app(PermissionRegistrar::class)->getPermissions();
+
+        $groupedPermissions = $permissions
+            ->groupBy('group_name')
+            ->mapWithKeys(function ($group, $groupName) {
+                return [$groupName => $group->pluck('name')->toArray()];
+            })
+            ->toArray();
+
+        dd($groupedPermissions);
+
+
+
         return view('welcome');
     }
 }
